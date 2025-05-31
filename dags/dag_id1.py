252d42eab9b5a595c9418237fead88ce18badaa6
matasuccias <memispuss1@gmail.com>
2025-05-31 12:03:12 -0500

    Update dag_id1.py

--- dags/dag_id1.py
+++ dags/dag_id1.py
@@ -1,9 +1,8 @@
-
+import pendulum
 from airflow import DAG
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-from airflow.utils.dates import days_ago
+from airflow.providers.snowflake.operators.snowflake import SnowflakeSqlApiOperator
 from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
+from datetime import timedelta, datetime
 
 default_args = {
     'owner': 'airflow',
@@ -26,14 +25,22 @@ def generate_sql_statements():
 with DAG(
     dag_id='create_snowflake_tables_every_15min',
     default_args=default_args,
-    schedule_interval='*/15 * * * *',
-    start_date=days_ago(1),
+    schedule='*/15 * * * *',  # <--- aquí el cambio
+    start_date=pendulum.now('UTC').subtract(days=1),
     catchup=False,
     tags=['snowflake', 'dynamic_tables'],
 ) as dag:
 
-    create_dynamic_tables = SnowflakeOperator(
-        task_id='create_dynamic_tables',
-        sql=generate_sql_statements(),
-        snowflake_conn_id='snowflake_conn_id', 
+    generate_sql = PythonOperator(
+        task_id='generate_sql',
+        python_callable=generate_sql_statements,
     )
+
+    create_dynamic_tables = SnowflakeSqlApiOperator(
+        task_id='create_dynamic_tables',
+        sql="{{ ti.xcom_pull(task_ids='generate_sql') }}",
+        snowflake_conn_id='snowflake_conn_id',
+    )
+
+    generate_sql >> create_dynamic_tables
+
